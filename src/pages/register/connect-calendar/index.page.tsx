0e05edc90d6263a777022167da378ff8fb43a5f5
Renato <renatomarquesteles@gmail.com>
2023-07-11 17:00:43 -0300

    chore: adds connect-calendar page

---
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -0,0 +1,36 @@
+import { Button, Heading, MultiStep, Text } from '@ignite-ui/react'
+import { ArrowRight } from 'phosphor-react'
+
+import { Container, Header } from '../styles'
+import { ConnectBox, ConnectItem } from './styles'
+
+export default function Register() {
+  return (
+    <Container>
+      <Header>
+        <Heading as="strong">Connect your Calendar!</Heading>
+        <Text>
+          Connect your calendar to automatically check for busy hours and new
+          events as they are scheduled.
+        </Text>
+
+        <MultiStep size={4} currentStep={2} />
+      </Header>
+
+      <ConnectBox>
+        <ConnectItem>
+          <Text>Google Calendar</Text>
+          <Button type="button" variant="secondary" size="sm">
+            Connect
+            <ArrowRight weight="bold" />
+          </Button>
+        </ConnectItem>
+
+        <Button type="button">
+          Next Step
+          <ArrowRight weight="bold" />
+        </Button>
+      </ConnectBox>
+    </Container>
+  )
+}
