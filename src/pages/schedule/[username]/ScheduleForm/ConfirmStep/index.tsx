995d7479be78eee07b54b2c92f220bd74170845d
Renato <renatomarquesteles@gmail.com>
2023-09-18 01:14:38 -0300

    chore: add confirm step form styles

---
+++ src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
@@ -0,0 +1,45 @@
+import { Button, Text, TextArea, TextInput } from '@ignite-ui/react'
+import { CalendarBlank, Clock } from 'phosphor-react'
+
+import { ConfirmForm, FormActions, FormHeader } from './styles'
+
+export function ConfirmStep() {
+  function handleConfirmScheduling() {}
+
+  return (
+    <ConfirmForm as="form" onSubmit={handleConfirmScheduling}>
+      <FormHeader>
+        <Text>
+          <CalendarBlank />
+          22th of September, 2023
+        </Text>
+        <Text>
+          <Clock />
+          18:00h
+        </Text>
+      </FormHeader>
+
+      <label>
+        <Text size="sm">Full name</Text>
+        <TextInput placeholder="Your name" />
+      </label>
+
+      <label>
+        <Text size="sm">Email Address</Text>
+        <TextInput type="email" placeholder="johndoe@example.com" />
+      </label>
+
+      <label>
+        <Text size="sm">Notes</Text>
+        <TextArea />
+      </label>
+
+      <FormActions>
+        <Button type="button" variant="tertiary">
+          Cancel
+        </Button>
+        <Button type="submit">Confirm</Button>
+      </FormActions>
+    </ConfirmForm>
+  )
+}
