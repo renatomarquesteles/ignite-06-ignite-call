5a7c008eec8e2607583faefe912359d872bf2e86
Renato <renatomarquesteles@gmail.com>
2023-09-18 04:05:32 -0300

    chore: check available times with no scheduling

---
+++ src/pages/api/users/[username]/availability.api.ts
@@ -0,0 +1,64 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import dayjs from 'dayjs'
+
+import { prisma } from '../../../../lib/prisma'
+
+export default async function handle(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method !== 'GET') return res.status(405).end()
+
+  const username = String(req.query.username)
+  const { date } = req.query
+
+  if (!date) return res.status(400).json({ message: 'Date not provided.' })
+
+  const user = await prisma.user.findUnique({ where: { username } })
+
+  if (!user) return res.status(400).json({ message: 'User does not exist.' })
+
+  const selectedDate = dayjs(String(date))
+
+  const isPastDate = selectedDate.endOf('day').isBefore(new Date())
+  if (isPastDate) return res.json({ possibleTimes: [], availableTimes: [] })
+
+  const userAvailability = await prisma.userTimeInterval.findFirst({
+    where: {
+      user_id: user.id,
+      week_day: selectedDate.get('day'),
+    },
+  })
+
+  if (!userAvailability)
+    return res.json({ possibleTimes: [], availableTimes: [] })
+
+  const { time_start_in_minutes, time_end_in_minutes } = userAvailability
+  const startHour = time_start_in_minutes / 60
+  const endHour = time_end_in_minutes / 60
+
+  const possibleTimes = Array.from({ length: endHour - startHour }).map(
+    (_, i) => startHour + i,
+  )
+
+  const blockedTimes = await prisma.scheduling.findMany({
+    select: { date: true },
+    where: {
+      user_id: user.id,
+      date: {
+        gte: selectedDate.set('hour', startHour).toDate(),
+        lte: selectedDate.set('hour', endHour).toDate(),
+      },
+    },
+  })
+
+  const availableTimes = possibleTimes.filter((time) => {
+    const isBlocked = blockedTimes.some(
+      (blockedTime) => blockedTime.date.getHours() === time,
+    )
+
+    return !isBlocked
+  })
+
+  return res.json({ possibleTimes, availableTimes })
+}
--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -1,6 +1,9 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
+import dayjs from 'dayjs'
 
 import { Calendar } from '../../../../../components/Calendar'
+import { api } from '../../../../../lib/axios'
 
 import {
   Container,
@@ -10,10 +13,34 @@ import {
   TimePickerList,
 } from './styles'
 
+interface Availability {
+  possibleTimes: number[]
+  availableTimes: number[]
+}
+
 export function CalendarStep() {
   const [selectedDate, setSelectedDate] = useState<Date | null>(null)
+  const [availability, setAvailability] = useState<Availability | null>(null)
+  const router = useRouter()
 
   const isDateSelected = !!selectedDate
+  const weekDay = selectedDate && dayjs(selectedDate).format('dddd')
+  const monthDay = selectedDate && dayjs(selectedDate).format('MMMM D')
+  const username = String(router.query.username)
+
+  useEffect(() => {
+    if (!selectedDate) return
+
+    api
+      .get(`/users/${username}/availability`, {
+        params: {
+          date: dayjs(selectedDate).format('YYYY-MM-DD'),
+        },
+      })
+      .then((response) => {
+        setAvailability(response.data)
+      })
+  }, [selectedDate, username])
 
   return (
     <Container isTimePickerOpen={isDateSelected}>
@@ -22,21 +49,18 @@ export function CalendarStep() {
       {isDateSelected && (
         <TimePicker>
           <TimePickerHeader>
-            tuesday <span>20th of september</span>
+            {weekDay} <span>{monthDay}</span>
           </TimePickerHeader>
 
           <TimePickerList>
-            <TimePickerItem>08:00</TimePickerItem>
-            <TimePickerItem>09:00</TimePickerItem>
-            <TimePickerItem>10:00</TimePickerItem>
-            <TimePickerItem>11:00</TimePickerItem>
-            <TimePickerItem>12:00</TimePickerItem>
-            <TimePickerItem>13:00</TimePickerItem>
-            <TimePickerItem>14:00</TimePickerItem>
-            <TimePickerItem>15:00</TimePickerItem>
-            <TimePickerItem>16:00</TimePickerItem>
-            <TimePickerItem>17:00</TimePickerItem>
-            <TimePickerItem>18:00</TimePickerItem>
+            {availability?.possibleTimes.map((hour) => (
+              <TimePickerItem
+                key={hour}
+                disabled={!availability.availableTimes.includes(hour)}
+              >
+                {String(hour).padStart(2, '0')}:00
+              </TimePickerItem>
+            ))}
           </TimePickerList>
         </TimePicker>
       )}
